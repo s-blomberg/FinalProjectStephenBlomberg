f33faac572897c50752433a359f94b7e247eda5e
Stephen Blomberg <s_blomberg@mail.fhsu.edu>
2024-12-02 03:18:40 -0600

    added templatetags/custom filters updated add_inventory_item updated edit_inventory_item updated base.html updated inventory_list.html updated login.html updated signup.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
-
 from .views import SignUpView
-
+from django.contrib.auth.views import LogoutView
+from django.urls import path
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
 ]
